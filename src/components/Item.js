565e3c8889e062fecffb43313245e2ee88ab351f
Irina Rogozhova <jouway@gmail.com>
2018-07-20 16:36:46 +0300

    simplified item markup by putting functions inside jsx

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 class Item extends React.Component {
-
-  handleCheckbox = () => { 
-    this.props.onCheck(this.props.id, this.props.isChecked); 
-  }
-
-  handleRemove = () => {
-    this.props.onRemove(this.props.id);
-  }
-
+  
   render() {
+    const { id, label, isChecked, onCheck, onRemove } = this.props;
     return (
-      <li className={this.props.isChecked ? 'completed' : ''}>
-        <input type="checkbox" className="toggle" onClick={this.handleCheckbox}/>
-        <label>{this.props.label}</label>
-        <button className="destroy" onClick={this.handleRemove}></button>
+      <li className={isChecked ? 'completed' : ''}>
+        <input 
+          type="checkbox" 
+          className="toggle" 
+          onClick={() => onCheck(id, isChecked)}
+        />
+        <label>{label}</label>
+        <button 
+          className="destroy" 
+          onClick={() => onRemove(id)}>
+        </button>
       </li>
     );
   }
