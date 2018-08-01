10880c358ca8de258266dfcce5d2f444107483a5
Irina Rogozhova <jouway@gmail.com>
2018-08-01 19:45:48 +0300

    tabs setup

--- src/components/footer/Tabs.js
+++ src/components/footer/Tabs.js
@@ -5,8 +5,27 @@ class Tabs extends Component {
 
   render () {
 
+    const tabArray = [
+      {id: 'tab-all', name: 'All'}, 
+      {id: 'tab-active', name: 'Active'}, 
+      {id: 'tab-completed', name: 'Completed'}
+    ];
+
+    const tabs = tabArray.map(pair =>
+      <li key={pair.id}>
+        <a id={pair.id} 
+          className={classnames('tablink', { selected: selectedId === pair.id })} 
+          >
+          {pair.name}
+        </a>
+      </li>
+    );
+
     return (
-      
+
+      <ul className='filters'>
+        {tabs}
+      </ul>
     );
   }
 }
