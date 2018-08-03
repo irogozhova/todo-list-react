aad13d91aaaea876dfd378f038e9a5c5dbdd44e2
Irina Rogozhova <jouway@gmail.com>
2018-08-03 16:48:43 +0300

    saving state to local storage (using Dan Abramov's tutorial)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from "redux";
 import todoReducer from "../reducers/index";
+import { loadState, saveState } from '../localStorage';
 
-const store = createStore(todoReducer);
+const persistedState = loadState();
+
+const store = createStore(
+  todoReducer,
+  persistedState
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
